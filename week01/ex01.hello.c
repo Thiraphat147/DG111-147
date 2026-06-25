b9f89f1dd0f5f05f4705011c52481e71a601be3a
Thiraphat <692110147@cmu.ac.th>
2026-06-25 15:45:35 +0700

    feat: add hello world program

---
+++ week01/ex01.hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีครับ/ค่ะ! ผม/หนู [ธีรภัทร หุ่นธานี]\n");
+    printf("รหัสนักศึกษา: 692110147\n");
+    printf("วิชา: DG111 Computer Programming");
+    return 0;
+}
